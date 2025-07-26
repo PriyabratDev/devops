83132c81388dc5edba64ef09c041b3919922a01d
PriyabratDev <priyabratomkumar@gmail.com>
2025-07-27 04:53:17 +0530

    tfsec fix 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -303,21 +303,21 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ip_range]
     description = "Allow HTTP from anywhere"
   }
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ip_range]
     description = "Allow SSH from anywhere"
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ip_range]
     description = "Allow all outbound"
   }
 }

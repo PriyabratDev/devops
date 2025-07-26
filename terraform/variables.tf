5081889d6b14b87ec9b1ff39bb118e24f90a5309
PriyabratDev <priyabratomkumar@gmail.com>
2025-07-27 04:12:16 +0530

    Code push after local pass

---
+++ terraform/variables.tf
@@ -0,0 +1,52 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "project_name" {
+  default = "dvscops"
+}
+
+variable "github_token" {
+  description = "GitHub token"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repo name"
+  type        = string
+}
+
+variable "github_branch" {
+  default = "main"
+}
+
+variable "bucket_name" {
+  description = "Artifact bucket name"
+  type        = string
+}
+
+variable "ami_id" {
+  default     = "ami-083e865b97bdf1c1b" # Amazon Linux 2023
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  type        = string
+}
+
+variable "instance_tag_key" {
+  default = "Name"
+}
+
+variable "instance_tag_value" {
+  default = "webserver"
+}
---
+++ test/pipeline_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformPipeline(t *testing.T) {
+  t.Parallel()
+
+  tf := &terraform.Options{
+    TerraformDir: "../terraform", // Adjust path as per your directory
+    VarFiles: []string{"terraform.tfvars"}, // Path to your variable file
+  }
+
+  defer func() {
+    if os.Getenv("SKIP_DESTROY") != "true" {
+      terraform.Destroy(t, tf)             // Cleanup after test if not skipped
+    }
+  }()
+
+  terraform.InitAndApply(t, tf)             
+  
+  pipelineName := terraform.Output(t, tf, "pipeline_name")
+  assert.NotEmpty(t, pipelineName)
+}

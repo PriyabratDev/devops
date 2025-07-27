9999b20d5c332510fc7aa34e58980d1810e26de2
PriyabratDev <priyabratomkumar@gmail.com>
2025-07-27 11:36:58 +0530

    Final Commit-

--- terraform/main.tf
+++ terraform/main.tf
@@ -224,10 +224,28 @@ resource "aws_iam_role" "codepipeline_role" {
     Statement = [{
       Effect = "Allow",
       Principal = {
-        Service = ["codepipeline.amazonaws.com", "codedeploy.amazonaws.com"]
+        Service = ["codebuild.amazonaws.com","codepipeline.amazonaws.com", "codedeploy.amazonaws.com"]
       },
       Action = "sts:AssumeRole"
     }]
+  })
+}
+resource "aws_iam_role_policy" "codepipeline_codebuild_policy" {
+  name = "${var.project_name}-codepipeline-codebuild-policy"
+  role = aws_iam_role.codepipeline_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "codebuild:StartBuild",
+          "codebuild:BatchGetBuilds"
+        ],
+        Resource = "arn:aws:codebuild:us-east-1:${data.aws_caller_identity.current.account_id}:project/${var.project_name}-build"
+      }
+    ]
   })
 }
 
@@ -346,7 +364,25 @@ resource "aws_codepipeline" "pipeline" {
     }
   }
 }
+resource "aws_iam_role_policy" "codepipeline_s3_policy" {
+  name = "${var.project_name}-codepipeline-s3-policy"
+  role = aws_iam_role.codepipeline_role.name
 
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ],
+        Resource = "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+      }
+    ]
+  })
+}
 resource "aws_iam_role" "ec2_role" {
   name = "${var.project_name}-ec2-role"
   assume_role_policy = jsonencode({

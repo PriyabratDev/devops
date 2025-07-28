c91095cfe7243b186c81e035038043c8f2b2d685
PriyabratDev <priyabratomkumar@gmail.com>
2025-07-28 22:37:44 +0530

    Final Commit and Build

--- project-task-1/src/App.jsx
+++ project-task-1/src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import './App.css'; // Optional: for styling
 
+function App() {
+  return (
+    <div className="App">
+      hi from devops
+    </div>
+  );
+}
+
+export default App;
